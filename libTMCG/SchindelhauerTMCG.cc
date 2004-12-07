fe0c740e27a920497d899795ee2530640eb83b1f
Heiko Stamer <HeikoStamer@gmx.net>
2004-12-07 20:42:58 +0000

    cosmetic

--- libTMCG/SchindelhauerTMCG.cc
+++ libTMCG/SchindelhauerTMCG.cc
@@ -13,7 +13,7 @@
 
      Dan Boneh: 'Simplified OAEP for the RSA and Rabin Functions', 2002
 
- Copyright (C) 2002, 2003, 2004 Heiko Stamer, <stamer@gaos.org>
+ Copyright (C) 2002, 2003, 2004 Heiko Stamer <stamer@gaos.org>
 
    This program is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
